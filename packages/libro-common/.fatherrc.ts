ae21b38810db97b2aa89c6ad20a15291b8d4cf1f
sunshinesmilelk <1176136681@qq.com>
2025-06-26 10:33:18 +0800

    fix: update json refs

--- packages/libro-common/.fatherrc.ts
+++ packages/libro-common/.fatherrc.ts
@@ -6,6 +6,7 @@ export default {
   extraBabelPlugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-transform-flow-strip-types'],
+    ['@babel/plugin-syntax-import-attributes', { deprecatedAssertSyntax: true }],
     ['@babel/plugin-transform-class-properties', { loose: true }],
     ['@babel/plugin-transform-private-methods', { loose: true }],
     ['@babel/plugin-transform-private-property-in-object', { loose: true }],
--- packages/libro-common/src/l10n/lang/index.ts
+++ packages/libro-common/src/l10n/lang/index.ts
@@ -1,7 +1,7 @@
 import type { LanguageBundles } from '../l10n/index.js';
 
-import langENUS from './bundle.l10n.en-US.json';
-import langZHCN from './bundle.l10n.zh-CN.json';
+import langENUS from './bundle.l10n.en-US.json' assert { type: 'json' };
+import langZHCN from './bundle.l10n.zh-CN.json' assert { type: 'json' };
 
 export const langBundles: LanguageBundles = {
   'zh-CN': langZHCN,
