66a2a0977ba68d3e767558f11a12bf6e224a1ec6
HellowVirgil <1135895008@qq.com>
2025-12-09 19:43:59 +0800

    fix(output): 修复输出唯一标识符

--- packages/libro-output/src/display-data-output/display-data-output-contribution.ts
+++ packages/libro-output/src/display-data-output/display-data-output-contribution.ts
@@ -3,8 +3,11 @@ import { inject, singleton } from '@difizen/libro-common/app';
 import { ViewManager } from '@difizen/libro-common/app';
 import type { IOutputOptions } from '@difizen/libro-core';
 import { OutputContribution } from '@difizen/libro-core';
+import { v4 } from 'uuid';
 
 import { DisplayDataOutputModel } from './display-data-output-model.js';
+
+const outputIdMap = new WeakMap<IOutput, string>();
 
 @singleton({ contrib: OutputContribution })
 export class DisplayDataOutputContribution implements OutputContribution {
@@ -23,11 +26,23 @@ export class DisplayDataOutputContribution implements OutputContribution {
     return this.viewManager.getOrCreateView(
       DisplayDataOutputModel,
       Object.assign(output, {
-        toJSON: () => ({
-          cellId: output.cell.id,
-          type: output.output.output_type,
-          keys: Object.keys((output.output as any).data || {}),
-        }),
+        toJSON: () => {
+          // 在 Jupyter 协议中，transient.display_id 用于标识可更新的显示数据
+          // 但是大多数输出（如静态图表、文本）没有 transient 字段。
+          const displayId = (output.output['transient'] as any)?.display_id;
+          let uniqueId = outputIdMap.get(output.output);
+
+          if (!displayId && !uniqueId) {
+            uniqueId = v4();
+            outputIdMap.set(output.output, uniqueId as string);
+          }
+
+          return {
+            _id: displayId || uniqueId,
+            cellId: output.cell.id,
+            type: output.output.output_type,
+          };
+        },
       }),
     );
   }
